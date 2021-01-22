918c78c0415bd752c958720861cfa99eaa202004
ishafran <isabelle.shafran@gmail.com>
2021-01-22 13:56:07 -0800

    change working of about me

--- src/components/splash.js
+++ src/components/splash.js
@@ -13,19 +13,19 @@ const Splash = () => {
       <span className="aboutme">ABOUT ME</span>
 
       <span className="splash-text">
-        In the midst of launching global ad campaigns for Land Rover the world
-        stopped and so did my work. In this lull I realized I was dissatisfied
+        In the midst of launching global ad campaigns for Land Rover, the world
+        stopped, and so did my work. In this Covid lull, I realized I was dissatisfied
         with my job and it was time to finally pursue something I always thought
         I would love: software engineering. Over the course of 4 months I went
         from launching ad campaigns on websites to building websites myself and
         I haven’t looked back since.
         <br />
         <br />
-        During my time at App Academy I gained extensive knowledge of languages
-        and frameworks such as React, Redux, Javascript, Ruby on Rails, SQL,
-        HTML, and CSS. I integrated those skills into two comprehensive
-        single-page applications: IntoTheGloss and DAYA, and one game utilizing
-        HTML Canvas and Vanilla JS Charmander’s Revenge.
+        I gained extensive knowledge of languages and frameworks such as 
+        React, Redux, Javascript, Ruby on Rails, SQL, HTML, and CSS.
+        I integrated those skills into two comprehensive single-page 
+        applications: IntoTheGloss and DAYA, and one game utilizing HTML Canvas and 
+        Vanilla JS Charmander’s Revenge.
       </span>
 
       <img alt="portfolio" className="profile_photo" src={ProfilePhoto} />
