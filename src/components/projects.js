660566b8dd1c107e85db3224e1ef6887c156e417
ishafran <isabelle.shafran@gmail.com>
2021-01-22 13:24:36 -0800

    add links to images

--- src/components/projects.js
+++ src/components/projects.js
@@ -15,13 +15,21 @@ const Projects = () => {
         <a className="headers" target="_blank" rel="noreferrer" href="https://isabelle-glossier.herokuapp.com/#/">IntoTheGloss</a>
             <span>Full stack web-application cloning the e-commerce site Glossier.</span>
             <span>Built with Ruby on Rails backend Framework and React Redux frontend.</span>
-            <img alt="intothegloss" className="intothegloss_image" src={IntoTheGloss}/>
+
+            <a target="_blank" rel="noreferrer" href="https://isabelle-glossier.herokuapp.com/#/">
+              <img alt="intothegloss" className="intothegloss_image" src={IntoTheGloss}/>
+            </a>
+
       </div>
       
       <div className="daya">
         <a className="headers" target="_blank" rel="noreferrer" href="http://daya-mern.herokuapp.com/#/">DAYA</a>
           <span>A more social take on the tradtional calendar built using the MERN stack.</span>
+
+          <a target="_blank" rel="noreferrer" href="http://daya-mern.herokuapp.com/#/">
             <img alt="daya" className="daya_image" src={DAYA}/>
+          </a>
+
       </div>
 
       
@@ -29,7 +37,10 @@ const Projects = () => {
       <div className="char">
         <a className="headers" target="_blank" rel="noreferrer" href="https://isabelleshafran.github.io/CharmandersRevenge/">Charmander's Revenge</a>
           <span>Endless runner game built using JavaScript and HTML Canvas</span>
-          <img alt="char" className="char_image" src={Char}/>
+
+          <a target="_blank" rel="noreferrer" href="https://isabelleshafran.github.io/CharmandersRevenge/">
+            <img alt="char" className="char_image" src={Char}/>
+          </a>
       </div>
 
     </div>
