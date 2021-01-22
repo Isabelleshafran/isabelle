d2b9d5dbd5ad4a84c50287a93fdeac0468778d48
ishafran <isabelle.shafran@gmail.com>
2021-01-22 13:05:45 -0800

    update styling

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -9,11 +9,11 @@ const Navbar = () => {
         <Link to={'/'}><div className="isabelle">ISABELLE SHAFRAN</div></Link>
 
         <div className="links">
-          <a target="_blank" rel="noreferrer" href="http://www.github.com/isabelleshafran">Github</a>
-          <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/isabelleshafran/">Linkedin</a>
-          <a target="_blank" rel="noreferrer" href="https://angel.co/u/isabelle-shafran">AngelList</a>
-          <Link to={'/projects'}>Projects</Link>
-          <a target="_blank" rel="noreferrer" href="mailto:isabelle.shafran@gmail.com">Email</a>
+          <a target="_blank" rel="noreferrer" href="http://www.github.com/isabelleshafran">GITHUB</a>
+          <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/isabelleshafran/">LINKEDIN</a>
+          <a target="_blank" rel="noreferrer" href="https://angel.co/u/isabelle-shafran">ANGELLIST</a>
+          <Link to={'/projects'}>PROJECTS</Link>
+          <a target="_blank" rel="noreferrer" href="mailto:isabelle.shafran@gmail.com">EMAIL</a>
         </div> 
       </div>
     );
